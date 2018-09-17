eb414ca41841404b636452921b8fb5cd819ffaf6
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-17 15:56:44 -0500

    Define and initialize left to empty array

--- qs.py
+++ qs.py
@@ -11,3 +11,6 @@ def quick_sort(array):
 	# Define and initialize pivot to last number in array to compare to other digits in array
 	pivot = array[arrlen - 1]
 
+	# Define and initialize left to empty array
+	left = [];
+
