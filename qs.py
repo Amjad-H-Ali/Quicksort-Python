c79364bd72e534144a431ccc2b573905fffc2ffc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-17 15:55:18 -0500

    Define and initialize length of array

--- qs.py
+++ qs.py
@@ -2,10 +2,12 @@ print("Quicksort!")
 
 # Function that takes in array as argument and returns sorted array
 def quick_sort(array):
+	# Define and initialize length of array
+	arrLen = len(array)
 	# If one element in array, return
-	if (len(array) < 2):
+	if (arrLen < 2):
 		return array
 
 	# Define and initialize pivot to last number in array to compare to other digits in array
-	pivot = len(array - 1)
+	pivot = array[arrlen - 1]
 
