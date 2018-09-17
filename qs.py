b5113d82ff537f534549e0db312688f3c1785bbe
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-17 15:51:10 -0500

    Added exit code to function for when recurion is added

--- qs.py
+++ qs.py
@@ -2,4 +2,7 @@ print("Quicksort!")
 
 # Function that takes in array as argument and returns sorted array
 def quick_sort(array):
-	return array
+	# If one element in array, return
+	if (len(array) < 2):
+		return array
+
